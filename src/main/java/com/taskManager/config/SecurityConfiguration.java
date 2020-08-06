c8f8a2183cea3924d2104643db05e97d8505486f
unknown <weotjqw@naver.com>
2020-08-06 11:43:09 +0900

    Front-end vuex modify

--- src/main/java/com/taskManager/config/SecurityConfiguration.java
+++ src/main/java/com/taskManager/config/SecurityConfiguration.java
@@ -1,4 +1,5 @@
 package com.taskManager.config;
+import com.taskManager.domain.common.security.AccessDeniedHandlerImpl;
 import com.taskManager.web.api.authenticate.AuthenticationFilter;
 import com.taskManager.web.api.authenticate.UserAuthenticationFailureHandler;
 import com.taskManager.web.api.authenticate.UserAuthenticationSuccessHandler;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -27,6 +29,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
       http
+        .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
+        .and()
         .authorizeRequests()
           .antMatchers(PUBLIC).permitAll() // PUBLIC 에 설정한 HTTP 요청은 누구나 허용
           .anyRequest().authenticated()    // PUBLIC 을 제외한 경로는 인증 필요 설정
@@ -78,5 +82,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
     @Bean
     public LogoutSuccessHandler logoutSuccessHandler() {
       return new UserLogoutSuccessHandler();
+    }
+
+    /*접근 불가 */
+    public AccessDeniedHandler accessDeniedHandler() {
+      return new AccessDeniedHandlerImpl();
     }
 }
